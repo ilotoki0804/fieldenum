2162058183202ba6be23d79d88ffe430f13c857d
ilotoki0804 <ilotoki0804@gmail.com>
2024-09-05 17:28:56 +0900

    Release 0.2.0

--- src/fieldenum/__init__.py
+++ src/fieldenum/__init__.py
@@ -2,3 +2,4 @@ from ._fieldenum import Unit, Variant, fieldenum, variant
 from .exceptions import unreachable
 
 __all__ = ["Unit", "Variant", "fieldenum", "unreachable", "variant"]
+__version__ = "0.2.0"
