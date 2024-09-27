c781a96ef0d123af6547f505bb1492d38a4611fc
ilotoki0804 <ilotoki0804@gmail.com>
2024-09-27 15:40:37 +0900

    Revert incorrect unsafe-fixes

--- src/fieldenum/exceptions.py
+++ src/fieldenum/exceptions.py
@@ -16,7 +16,7 @@ class Unreachable(BaseException):
 def unreachable(value=_MISSING) -> _NoReturn:
     raise Unreachable(
         "This code is meant to be unreachable, but somehow the code reached here. "
-        f"Address developers to fix the issue. Value: {value!r}" * (value is not _MISSING)
+        "Address developers to fix the issue." + f" Value: {value!r}" * (value is not _MISSING)
     )
 
 
